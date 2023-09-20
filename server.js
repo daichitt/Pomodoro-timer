7f447c9d33e84757cedfb3ceb0f90d847f6fb367
daichit <gene.sixteen.000925@outlook.jp>
2023-09-20 19:58:10 +0700

    first commit on dev branch

--- server.js
+++ server.js
@@ -1,1 +1,17 @@
-console.log("node js ")
+const express = require("express")
+const app = express();
+
+const mongoose = require("mongoose")
+
+// app.listen(3000, () => {
+//     console.log("コンソール　起動しました")
+// })
+
+
+
+
+// app.listen(3000);
+
+app.listen(3000, () => {
+    console.log("コンソール　起動しました")
+})
